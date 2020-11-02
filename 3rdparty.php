457a78bde65f6a91718ad54a8f2fcac50b9b217f
Gurri Singh <gurri.singh003@gmail.com>
2020-11-02 12:42:53 +0530

    remotelogin

---
+++ 3rdparty.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <script src="https://www.myclick123.com/eng/web/tt/js/authentication.js" type="text/javascript"></script>
+</head>
+<body>
+    <div class="container">
+        <br/>
+                <button type="button" class="btn btn-primary btn-block authenticate-login">Launch Window DOM Popup</button>
+                <button type="button" class="btn btn-primary btn-block authenticate-login1">Launch Window Popup</button>
+    </div>
+</body>
+</html>
